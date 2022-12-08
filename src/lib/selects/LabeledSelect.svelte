f28b73041cfa7da351926e2de87ac1f353bc8cdf
AcrylicShrimp <led789zxpp@naver.com>
2022-12-09 02:19:01 +0900

    feat: apply rounded select inputs.

--- src/lib/selects/LabeledSelect.svelte
+++ src/lib/selects/LabeledSelect.svelte
@@ -5,7 +5,7 @@
 <label class="flex flex-row items-center">
   <p>{label}:</p>
   <span class="flex-none inline-block w-2" />
-  <select class="text-0/5 bg-4/5" on:change>
+  <select class="text-0/5 bg-4/5 rounded" on:change>
     <slot />
   </select>
 </label>
