148013eede5638781b7f7fc51b9f070cc6223c9f
AcrylicShrimp <led789zxpp@naver.com>
2025-10-11 00:59:30 +0900

    test: add a test case

--- packages/dm_api/test/dm_api_test.dart
+++ packages/dm_api/test/dm_api_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:dm_api/dm_api.dart';
 import 'package:dm_api/dm_api_cookie_jar.dart';
+import 'package:dm_api/dm_api_product.dart';
 import 'package:dm_api/dm_api_purchased_product.dart';
 import 'package:test/test.dart';
 
@@ -47,5 +48,22 @@ void main() {
 
     final purchasedProducts = await dmApi.getPurchasedProducts();
     expect(purchasedProducts, isA<List<DmApiPurchasedProduct>>());
+  });
+
+  test("should get products", () async {
+    final username = Platform.environment['TEST_USERNAME'];
+    final password = Platform.environment['TEST_PASSWORD'];
+
+    if (username == null || password == null) {
+      throw Exception('TEST_USERNAME and TEST_PASSWORD must be set');
+    }
+
+    final dmApi = DmApi(Dio(), DmApiCookieJar.empty());
+    await dmApi.login(username, password);
+
+    final purchasedProducts = await dmApi.getPurchasedProducts();
+    final productIds = purchasedProducts.map((e) => e.id).toList();
+    final products = await dmApi.getProducts(productIds.sublist(0, 10));
+    expect(products, isA<List<DmApiProduct>>());
   });
 }
